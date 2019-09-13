03086bcd3fb2fa56b353ee38a8b87967a39a9e87
Martin Miglio <marmig0404@gmail.com>
2019-09-13 15:16:23 -0400

    Updates

--- precache-manifest.cf8ce415317452cb28c9a913463d3603.js
+++ precache-manifest.cf8ce415317452cb28c9a913463d3603.js
@@ -1,26 +1,26 @@
 self.__precacheManifest = [
   {
-    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
-    "url": "/portfolio/static/media/logo.ee7cd8ed.svg"
+    "revision": "8035205cc3f219b4b569f87bb8107c8f",
+    "url": "/portfolio/static/media/logo.8035205c.svg"
   },
   {
     "revision": "53ebc025fca807d4dbea",
     "url": "/portfolio/static/js/runtime~main.53ebc025.js"
   },
   {
-    "revision": "d2b5ee379721f4155ece",
-    "url": "/portfolio/static/js/main.d2b5ee37.chunk.js"
+    "revision": "71da5ed1c79078b76f7c",
+    "url": "/portfolio/static/js/main.71da5ed1.chunk.js"
   },
   {
     "revision": "9f8f6cd94fde2671caac",
     "url": "/portfolio/static/js/2.9f8f6cd9.chunk.js"
   },
   {
-    "revision": "d2b5ee379721f4155ece",
+    "revision": "71da5ed1c79078b76f7c",
     "url": "/portfolio/static/css/main.ae358d00.chunk.css"
   },
   {
-    "revision": "2294e77a1ebd3d91c9ddfd6e34dbfcfa",
+    "revision": "c34a14dc6347f437504b1f380111165f",
     "url": "/portfolio/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.7ac6798f65bbd17b549ea622b271e6a2.js"
+  "/portfolio/precache-manifest.cf8ce415317452cb28c9a913463d3603.js"
 );
 
 workbox.clientsClaim();
