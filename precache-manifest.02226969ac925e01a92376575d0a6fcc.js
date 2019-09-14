6e0ae87df77a79e7a631b7397a56c12bf7a651c3
Martin Miglio <marmig0404@gmail.com>
2019-09-14 00:30:24 -0400

    Updates

---
+++ precache-manifest.02226969ac925e01a92376575d0a6fcc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ca996c86f2c9ea4a86d589305b4c671",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "17a93e81108dac239195",
+    "url": "/portfolio/static/css/main.f7b691db.chunk.css"
+  },
+  {
+    "revision": "841b39e931dd7cea6e25",
+    "url": "/portfolio/static/js/2.467027d4.chunk.js"
+  },
+  {
+    "revision": "17a93e81108dac239195",
+    "url": "/portfolio/static/js/main.4152db42.chunk.js"
+  },
+  {
+    "revision": "255cf37ac81f61801b31",
+    "url": "/portfolio/static/js/runtime~main.36963274.js"
+  },
+  {
+    "revision": "09bbc8b8f493b1dd8c7bd91cac6a63aa",
+    "url": "/portfolio/static/media/logo.09bbc8b8.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.fa4fab4afe98f226a64ec995bebc50d1.js"
+  "/portfolio/precache-manifest.02226969ac925e01a92376575d0a6fcc.js"
 );
 
 self.addEventListener('message', (event) => {
