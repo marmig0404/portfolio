857558ebbb5bab9237df672e27e3e32d433100e3
Martin Miglio <marmig0404@gmail.com>
2020-06-11 02:07:00 -0400

    Updates

---
+++ precache-manifest.1d03850549e53ddc9ecda1149d9e8c1d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b189f41e1983de6e781001a15a615d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e3d95a92edbe0252d24",
+    "url": "/static/css/main.6524b2b0.chunk.css"
+  },
+  {
+    "revision": "f6a87534669125108187",
+    "url": "/static/js/2.1ad28a05.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.1ad28a05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e3d95a92edbe0252d24",
+    "url": "/static/js/main.0ac7920f.chunk.js"
+  },
+  {
+    "revision": "967a46d8b67e4e01db1f",
+    "url": "/static/js/runtime-main.7e4b9196.js"
+  },
+  {
+    "revision": "754bec9262c9405f0f0b486a0372789a",
+    "url": "/static/media/DDCLogo1.754bec92.png"
+  },
+  {
+    "revision": "891e0efca2924f65adfaae39bc8a0f36",
+    "url": "/static/media/PortfolioLogo.891e0efc.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.b32075656602896ab722adbcabefaa74.js"
+  "/precache-manifest.1d03850549e53ddc9ecda1149d9e8c1d.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
